eb01793b237380c823a55344030d6bcfa4b93d75
Sandesh Pokhrel <sandeshpokhrel54@gmail.com>
2022-09-06 15:52:08 +0545

    expected goals sd and expected points

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Xpoints
-- Take XG values of each shot of two teams as input
-- simulate a number of games (about 100k) and their win, loose and draw percentage
-- calculate expected points
-- include standard deviation in quality of chances
+- Take XG values of each shot of two teams as input: done
+- simulate a number of games (about 100k) and their win, loose and draw percentage: done
+- calculate expected points: done
+- include standard deviation in quality of chances: done
+- simulate including standard deviation
--- xpoints.py
+++ xpoints.py
@@ -1,4 +1,5 @@
 import random
+from signal import NSIG
 
 Team1 = [0.02,0.02,0.03,0.04,0.04,0.05,0.06,0.07,0.09,0.10,0.12,0.13,0.76] # home team
 team1XG  = sum(Team1)
@@ -10,7 +11,7 @@ team2XG  = sum(Team2)
 
 
 def scorecount(team):
-    
+
     goals = 0
     for i in team:
         rand = random.randint(1,1000)/1000
@@ -21,14 +22,14 @@ def scorecount(team):
     return goals
 
 
-N  = 100000 # number of simulations
-def play(N):
+N_SIMULATIONS  = 100000 # number of simulations
+def play(N_SIMULATIONS):
 
     homewins = 0
     awaywins = 0
     draws = 0
 
-    for i in range(N):
+    for i in range(N_SIMULATIONS):
         goalsHome = scorecount(Team1)
         goalsAway = scorecount(Team2)
         # print(goalsHome, goalsAway)
@@ -43,10 +44,25 @@ def play(N):
     return homewins, draws, awaywins
 
 
-homewins,draws,awaywins = play(N)
+homewins,draws,awaywins = play(N_SIMULATIONS)
 
-print(homewins/N,draws/N,awaywins/N)
+shots_team_1 = len(Team1)
+shots_team_2 = len(Team2)
+meanTeam1 = team1XG/shots_team_1
+meanTeam2 = team2XG/shots_team_2
 
-home_expected_points = homewins/N*3 + draws/N #homewinpercentage * 3 points + drawpercentage * 1 point = expected points
-away_expected_points = awaywins/N*3 + draws/N #awaywinpercentage * 3 points + drawpercentage * 1 point = expected points
-print(home_expected_points, away_expected_points)
+sdteam1 = round((sum((i-meanTeam1)**2 for i in Team1)/shots_team_1)**0.5,3)
+sdteam2 = round((sum((i-meanTeam2)**2 for i in Team2)/shots_team_2)**0.5,3)
+
+print(homewins/N_SIMULATIONS,draws/N_SIMULATIONS,awaywins/N_SIMULATIONS)
+home_win_rate = homewins/N_SIMULATIONS
+draw_rate = draws/N_SIMULATIONS
+away_win_rate = awaywins/N_SIMULATIONS
+
+home_expected_points = round(home_win_rate*3 + draw_rate,3) #homewinpercentage * 3 points + drawpercentage * 1 point = expected points
+away_expected_points = round(away_win_rate*3 + draw_rate,3) #awaywinpercentage * 3 points + drawpercentage * 1 point = expected points
+print(f'Home Team: {team1XG} +- {sdteam1}, Away Team:{team2XG} +- {sdteam2}')
+
+print(f'Home Team: {home_expected_points}, Away Team:{away_expected_points}')
+
+#calculate expected points after standard deviation
