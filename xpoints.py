df4fdf5efd39ed5905b429514e1508e1a0c23b9b
Sandesh Pokhrel <sandeshpokhrel54@gmail.com>
2022-09-22 22:59:13 +0545

    Finally, the correct version of deviation

--- xpoints.py
+++ xpoints.py
@@ -2,11 +2,9 @@ import random
 
 Team1 = [0.02,0.02,0.03,0.04,0.04,0.05,0.06,0.07,0.09,0.10,0.12,0.13,0.76] # home team
 team1XG  = sum(Team1)
-# print(team1XG)
 
 Team2 = [0.01,0.02,0.02,0.02,0.03,0.05,0.05,0.05,0.06,0.22,0.30,0.43,0.48,0.63] #away team
 team2XG  = sum(Team2)
-# print(team2XG)
 
 team_coin = [0.5,0.5,0.5,0.5]
 coinXG = sum(team_coin)
@@ -23,8 +21,7 @@ westham = [0.03, 0.08, 0.03, 0.94, 0.33]
 westhamXG = sum(westham)
 
 
-
-N_SIMULATIONS  = 10000 # number of simulations
+N_SIMULATIONS  = 100000 # number of simulations
 PRECISION = 1000
 
 def scorecount(team):
@@ -45,10 +42,15 @@ def play(Team1, Team2, N_SIMULATIONS):
     awaywins = 0
     draws = 0
 
+    home_goals_count = {}
+    away_goals_count = {}
+
     for i in range(N_SIMULATIONS):
         goalsHome = scorecount(Team1)
         goalsAway = scorecount(Team2)
         # print(goalsHome, goalsAway)
+        home_goals_count.update({goalsHome:home_goals_count.get(goalsHome,0)+1})
+        away_goals_count.update({goalsAway:away_goals_count.get(goalsAway,0)+1})
 
         if(goalsHome>goalsAway):
             homewins = homewins + 1
@@ -57,26 +59,7 @@ def play(Team1, Team2, N_SIMULATIONS):
         else:
             awaywins = awaywins + 1
 
-    return homewins, draws, awaywins
-
-
-def xpoints(Team1, Team2,N_SIMULATIONS):
-    
-    homewins, draws, awaywins = play(Team1, Team2,N_SIMULATIONS)
-    
-    # print("homeWins: ", homewins)
-    # print("draws:", draws)
-    # print("awayWins:", awaywins)
-    print(homewins/N_SIMULATIONS,draws/N_SIMULATIONS,awaywins/N_SIMULATIONS)
-
-    home_win_rate = homewins/N_SIMULATIONS
-    draw_rate = draws/N_SIMULATIONS
-    away_win_rate = awaywins/N_SIMULATIONS
-    xpointsHome = round(home_win_rate*3 + draw_rate,3) #homewinpercentage * 3 points + drawpercentage * 1 point = expected points
-    xpointsAway = round(away_win_rate*3 + draw_rate,3) #awaywinpercentage * 3 points + drawpercentage * 1 point = expected points
-    
-    return xpointsHome, xpointsAway
-
+    return homewins, draws, awaywins,home_goals_count, away_goals_count
 
 #standard deviation on the quality of chances each team creates
 #another useful factor might be the standard deviation of the goals scored by each team which is
@@ -91,33 +74,60 @@ def deviation(Team1, Team2):
     sdteam2 = round((sum((i-meanTeam2)**2 for i in Team2)/shots_team_2)**0.5,3)
     return sdteam1,sdteam2
 
-# def deviationGoals(Team1, Team2):
-#     #number of shots each team takes is the most number of goals they can score
-#     #0 is the least number of goals the team can score
-#     #so, find standard deviation of numbers from 0 to number of shots taken for each team
-#     n1 = len(Team1)
-#     n2 = len(Team2)
-#     sdteam1 = ((n1*n1 + 2*n1)/12)**0.5
-#     sdteam2 = ((n2*n2 + 2*n2)/12)**0.5
-#     return sdteam1, sdteam2
+def deviationGoals(team1_goal_counts, team2_goal_counts):
+    #for x ranging from 0 to number of shots
+    #find the probability of scoring x goals
+    #and calculate standard deviation
+    team1_goal_counts_prob = {k:v/N_SIMULATIONS for k,v in team1_goal_counts.items()}
+    team2_goal_counts_prob = {k:v/N_SIMULATIONS for k,v in team2_goal_counts.items()}
+
+    #now that the probailites are known
+    #find mean and standard deviation of goals scored
+    meanTeam1 = sum([k*v for k,v in team1_goal_counts_prob.items()])
+    meanTeam2 = sum([k*v for k,v in team2_goal_counts_prob.items()])
+    sdteam1 = round((sum((k-meanTeam1)**2*v for k,v in team1_goal_counts_prob.items()))**0.5,3)
+    sdteam2 = round((sum((k-meanTeam2)**2*v for k,v in team2_goal_counts_prob.items()))**0.5,3)
+
+    # print(team1_goal_counts_prob, team2_goal_counts_prob)
+    return sdteam1, sdteam2
+
+
+def xpoints(Team1, Team2,N_SIMULATIONS):
+    
+    homewins, draws, awaywins, home_goals_count, away_goals_count  = play(Team1, Team2,N_SIMULATIONS)
+    
+    # print(home_goals_count, away_goals_count)
+    # print("homeWins: ", homewins)
+    # print("draws:", draws)
+    # print("awayWins:", awaywins)
+    # print(homewins/N_SIMULATIONS,draws/N_SIMULATIONS,awaywins/N_SIMULATIONS)
+    
+    team1dev, team2dev = deviationGoals(home_goals_count, away_goals_count)
+    
+    home_win_rate = homewins/N_SIMULATIONS
+    draw_rate = draws/N_SIMULATIONS
+    away_win_rate = awaywins/N_SIMULATIONS
+    xpointsHome = round(home_win_rate*3 + draw_rate,3) #homewinpercentage * 3 points + drawpercentage * 1 point = expected points
+    xpointsAway = round(away_win_rate*3 + draw_rate,3) #awaywinpercentage * 3 points + drawpercentage * 1 point = expected points
+    
+    return xpointsHome, xpointsAway, team1dev, team2dev
 
 
 def calcXPoints(Team1, Team2, N_SIMULATIONS):
-    xpointsHome, xpointsAway = xpoints(Team1, Team2,N_SIMULATIONS)
-    sdteam1,sdteam2 = deviation(Team1, Team2)
-    print(f'Home Team: {sum(Team1)} +- {sdteam1}, Away Team:{sum(Team2)} +- {sdteam2}')
+    xpointsHome, xpointsAway, team1dev, team2dev = xpoints(Team1, Team2,N_SIMULATIONS)
+    # sdteam1,sdteam2 = deviation(Team1, Team2)
+    print(f'Home Team: {sum(Team1)} +- {team1dev}, Away Team:{sum(Team2)} +- {team2dev}')
     print(f'Home Team: {xpointsHome}, Away Team:{xpointsAway}')
+    # print(f'psst Dev team1: {team1dev}, psst Dev team2:{team2dev}')
 
-    return xpointsHome, xpointsAway, sdteam1, sdteam2
+    return xpointsHome, xpointsAway, team1dev, team2dev
 
 
-# homexp, awayexp, sdteam1, sdteam2 = calcXPoints(Team1, Team2, N_SIMULATIONS)
+homexp, awayexp, sdteam1, sdteam2 = calcXPoints(Team1, Team2, N_SIMULATIONS)
 coinxp, dicexp, sdteam1, sdteam2 = calcXPoints(team_coin, team_dice, N_SIMULATIONS)
 
 #to compare understat's xp and the one i calculated
-# chelseaxp, westhamxp, sdteam1, sdteam2 = calcXPoints(chelsea, westham, N_SIMULATIONS)
-
-#calculate expected points after standard deviation of number of goals each team can score
+chelseaxp, westhamxp, sdteam1, sdteam2 = calcXPoints(chelsea, westham, N_SIMULATIONS)
 
 #expected goals not added but conditionally added(in cases of chances coming off of rebounds)? danny page medium
 
